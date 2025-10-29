c07020e771559c33a2b1c63a58ba0151e1cee06d
Fety <warpfinanceeth@gmail.com>
2025-10-29 02:53:22 -0700

    Jingo

--- src/entities/baseCurrency.ts
+++ src/entities/baseCurrency.ts
@@ -11,7 +11,7 @@ export abstract class BaseCurrency {
    */
   public abstract readonly isNative: boolean
   /**
-   * Returns whether the currency is a token that is usable in Pegasys without wrapping
+   * Returns whether the currency is a token that is usable in Jingo without wrapping
    */
   public abstract readonly isToken: boolean
 
@@ -56,8 +56,8 @@ export abstract class BaseCurrency {
   public abstract equals(other: Currency): boolean
 
   /**
-   * Return the wrapped version of this currency that can be used with the Pegasys contracts. Currencies must
-   * implement this to be used in Pegasys
+   * Return the wrapped version of this currency that can be used with the Jingo contracts. Currencies must
+   * implement this to be used in Jingo
    */
   public abstract get wrapped(): Token
 }
